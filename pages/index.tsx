f92a92cfa2e9e4aa8b8fd572c03d34d5153a54cf
Phillip Maier <pmaier983@gmail.com>
2020-12-04 22:07:40 -0500

    Small page load message change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import React from "react"
 
 const Home: React.FC = () => {
-  return <div>Hello world! This is also Gaulish!</div>
+  return (
+    <div>
+      Hello world! This is also Gaulish! I&apos;ve been working on the SQL of
+      gaulish, so low commit velocity
+    </div>
+  )
 }
 
 export default Home
