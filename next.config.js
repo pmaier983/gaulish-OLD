a6c43d4f55e062e4549eb008ba49b9bd8c2e768d
Phillip Maier <pmaier983@gmail.com>
2020-12-13 23:16:10 -0500

    Remove All next.js stuff from frontend

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// Allow bundle analyzer to work
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-})
-
-module.exports = withBundleAnalyzer({})
--- theme/theme.ts
+++
@@ -1,5 +0,0 @@
-import { colors } from "./colors"
-export const theme = {
-  primaryFontFamily: "system-ui, sans-serif",
-  colors,
-}
