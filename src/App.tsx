31cf315449f804c36dca67cfb2ef9a13e7f753fa
Phillip Maier <pmaier983@gmail.com>
2021-04-26 07:56:25 -0400

    Setup Vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledThing = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+`
+
+function App() {
+  return <StyledThing />
+}
+
+export default App
