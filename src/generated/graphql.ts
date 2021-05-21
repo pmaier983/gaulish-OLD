935ec6783b9291fd821daefdb71e895ccbac41b6
Phillip Maier <pmaier983@gmail.com>
2021-05-21 16:59:17 -0400

    Add api types via codegen

---
+++ src/generated/graphql.ts
@@ -0,0 +1,56 @@
+export type Maybe<T> = T | null
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K]
+}
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]?: Maybe<T[SubKey]> }
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]: Maybe<T[SubKey]> }
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
+}
+
+export type Node = {
+  id: Scalars["ID"]
+}
+
+export type Point = {
+  x?: Maybe<Scalars["Int"]>
+  y?: Maybe<Scalars["Int"]>
+}
+
+export type Query = {
+  __typename?: "Query"
+  getAllTiles?: Maybe<Array<Maybe<Tile>>>
+  getTilesWithinRectangle?: Maybe<Array<Maybe<Tile>>>
+  getTilesAroundTile?: Maybe<Array<Maybe<Tile>>>
+  getTileByID?: Maybe<Tile>
+  verifyToken: Scalars["Boolean"]
+}
+
+export type QueryGetTilesWithinRectangleArgs = {
+  pointA?: Maybe<Point>
+  pointB?: Maybe<Point>
+}
+
+export type QueryGetTilesAroundTileArgs = {
+  point?: Maybe<Point>
+  radius?: Maybe<Scalars["Int"]>
+}
+
+export type QueryGetTileByIdArgs = {
+  tileId?: Maybe<Scalars["Int"]>
+}
+
+export type Tile = Node & {
+  __typename?: "Tile"
+  id: Scalars["ID"]
+  tile_id?: Maybe<Scalars["Int"]>
+  x?: Maybe<Scalars["Int"]>
+  y?: Maybe<Scalars["Int"]>
+}
