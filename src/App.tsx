1cd7e5626f01e235460470a6fd5fb7133bdea7dd
Phillip ED Maier <pmaier983@gmail.com>
2021-09-12 20:41:46 -0700

    Add Basic HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,22 @@
 import type React from "react"
-import styled from "styled-components"
 
 import { AppWrapper } from "@/AppWrapper"
 import { LoginPage } from "@/pages/LoginPage"
+import { HomePage } from "@/pages/HomePage"
 import { useUserContext } from "@/context/UserProvider"
-import { useSubscription } from "@/hooks/useSubscription"
-import { gql } from "graphql-request"
-
-const StyledWrapper = styled.div`
-  height: 100%;
-  background-color: ${({ theme }) => theme.color.gray[300]};
-`
 
 // TODO: setup 404 page
 // handle Shared server <-> client types
 // TODO remove lodash from the app
+// TODO: bump to husky 7.X
 const App = () => {
-  const { isLoggedIn, user, logoutUser } = useUserContext()
+  const { isLoggedIn } = useUserContext()
 
-  const data = useSubscription({
-    query: gql`
-      subscription {
-        globalChat {
-          text
-          username
-        }
-      }
-    `,
-  })
-
-  return (
-    <div>
-      Hello
-      {JSON.stringify(data)}
-    </div>
-  )
   if (!isLoggedIn) {
     return <LoginPage />
   }
 
-  return (
-    <StyledWrapper>
-      <h1>Hello</h1>
-      <div>{JSON.stringify(user)}</div>
-      <button onClick={logoutUser}>Logout</button>
-    </StyledWrapper>
-  )
+  return <HomePage />
 }
 
 const WrappedApp: React.FC = () => (
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,29 @@
+import { gql } from "graphql-request"
+import styled from "styled-components"
+
+import { useSubscription } from "@/hooks/useSubscription"
+
+const StyledWrapper = styled.div`
+  height: 100%;
+  background-color: ${({ theme }) => theme.color.gray[300]};
+`
+
+export const HomePage = () => {
+  const data = useSubscription({
+    query: gql`
+      subscription {
+        globalChat {
+          text
+          username
+        }
+      }
+    `,
+  })
+
+  return (
+    <StyledWrapper>
+      Hello
+      {JSON.stringify(data)}
+    </StyledWrapper>
+  )
+}
