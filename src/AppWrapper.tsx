5f1f4207b74f8a5d24b6ded4bee87c3c2dde5f09
Phillip ED Maier <pmaier983@gmail.com>
2021-09-15 20:19:21 -0700

    disable global websocket as it causes catch errors

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -4,15 +4,14 @@ import { QueryProvider } from "@/context/QueryProvider"
 import { ThemeWrapper } from "@/context/ThemeProvider"
 import { UserProvider } from "@/context/UserProvider"
 import { GlobalStyle } from "@/utils/globalStyles"
-import { useSubscription } from "./hooks/useSubscription"
-import { gql } from "graphql-request"
 
 // TODO is there a better way to type this (if you need children?)
 // TODO figure out React.PropsWithChildren
 const AppWrapper: React.FC = ({ children }) => {
+  // TODO: how to open up a websocket without an actual query?
   // this empty subscription will open the websocket connection and keep it open
   // until the connection is closed
-  useSubscription({ query: gql`` })
+  // useSubscription({ query: gql`` })
   return (
     <QueryProvider>
       <UserProvider>
