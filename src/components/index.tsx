555a726df27c1f678bc74cc6b634a60fcdb0aa41
Phillip ED Maier <pmaier983@gmail.com>
2021-09-30 19:00:23 -0700

    Add Index and HomePage organization

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+// TODO: anyway to automate this?
+export * from "./ChatBox"
+export * from "./Footer"
+export * from "./Grid"
+export * from "./Header"
+export * from "./Map"
+export * from "./SidebarLeft"
+export * from "./SidebarRight"
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,49 +1,26 @@
-import { useState } from "react"
 import styled from "styled-components"
 
-import { MixedCheckbox } from "@reach/checkbox"
-import { toggleWebsocketsEnabled } from "@/utils/helperFunctions"
-import { LOCAL_STORAGE_KEYS } from "@/utils/enums"
-import { ChatBox } from "@/components/ChatBox"
-import { useUserContext } from "@/context/UserProvider"
-import { Grid } from "@/components/Grid"
+import { Header, Footer, SidebarLeft, SidebarRight, Map } from "@/components"
 
 const StyledWrapper = styled.div`
   display: grid;
   height: 100%;
-  grid-template-columns: minmax(100px, 1fr) minmax(10px, 2fr) minmax(100px, 1fr);
-  grid-template-rows: minmax(100px, 1fr) minmax(10px, 2fr) minmax(10px, 1fr);
+  grid-template-columns: 200px 1fr 200px;
+  grid-template-rows: 100px 1fr 200px;
+  grid-template-areas:
+    "header header header"
+    "sidebarLeft map sidebarRight"
+    "footer footer footer";
 `
 
 export const HomePage = () => {
-  const { logoutUser } = useUserContext()
-  const [checked, setChecked] = useState(
-    !!window.localStorage.getItem(LOCAL_STORAGE_KEYS.HAS_WEBSOCKET_ENABLED)
-  )
-
   return (
     <StyledWrapper>
-      {/* Enable Chat
-      <MixedCheckbox
-        checked={checked}
-        onChange={({ target: { checked } }) => {
-          setChecked(checked)
-          toggleWebsocketsEnabled(checked)
-        }}
-      />
-      {checked && <ChatBox />}
-      <button onClick={logoutUser}>Logout</button> */}
-      {/* <Grid /> */}
-
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
+      <Header />
+      <SidebarLeft />
+      <Map />
+      <SidebarRight />
+      <Footer />
     </StyledWrapper>
   )
 }
