f00bc30d5742bda778f79eb842cb5a4011966da2
Phillip ED Maier <pmaier983@gmail.com>
2021-10-02 18:31:47 -0700

    Add Increase/Decrease cell size Buttons

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -3,24 +3,36 @@ import {
   GridChildComponentProps,
 } from "react-window"
 import AutoSizer from "react-virtualized-auto-sizer"
+import styled from "styled-components"
+
+import { useGridContext } from "@/context/GridProvider"
+
+const StyledWrapper = styled.div`
+  grid-area: grid;
+`
 
 export const Cell = ({ style }: GridChildComponentProps) => {
   return <div style={style}>I am cell</div>
 }
 
-export const Grid = () => (
-  <AutoSizer>
-    {({ height, width }) => (
-      <VirtualizedGrid
-        height={height}
-        width={width}
-        columnCount={1000}
-        columnWidth={100}
-        rowCount={1000}
-        rowHeight={100}
-      >
-        {Cell}
-      </VirtualizedGrid>
-    )}
-  </AutoSizer>
-)
+export const Grid = () => {
+  const { cellSize } = useGridContext()
+  return (
+    <StyledWrapper>
+      <AutoSizer>
+        {({ height, width }) => (
+          <VirtualizedGrid
+            height={height}
+            width={width}
+            columnCount={100}
+            columnWidth={cellSize}
+            rowCount={100}
+            rowHeight={cellSize}
+          >
+            {Cell}
+          </VirtualizedGrid>
+        )}
+      </AutoSizer>
+    </StyledWrapper>
+  )
+}
--- src/components/SidebarRight.tsx
+++ src/components/SidebarRight.tsx
@@ -1,9 +1,30 @@
 import styled from "styled-components"
 
+import { useGridContext } from "@/context/GridProvider"
+
 const StyledWrapper = styled.div`
   grid-area: sidebarRight;
 `
 
 export const SidebarRight = () => {
-  return <StyledWrapper>SidebarRight</StyledWrapper>
+  const { dispatchGridAction, GRID_ACTIONS } = useGridContext()
+
+  return (
+    <StyledWrapper>
+      <button
+        onClick={() =>
+          dispatchGridAction({ type: GRID_ACTIONS.INCREASE_CELL_SIZE })
+        }
+      >
+        Increase Cell Size
+      </button>
+      <button
+        onClick={() =>
+          dispatchGridAction({ type: GRID_ACTIONS.DECREASE_CELL_SIZE })
+        }
+      >
+        Decrease Cell Size
+      </button>
+    </StyledWrapper>
+  )
 }
