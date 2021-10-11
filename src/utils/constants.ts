39d10529bc348fd865afca7c100e79136169e8c3
Phillip ED Maier <pmaier983@gmail.com>
2021-10-10 20:42:18 -0700

    Remove unused constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,8 @@
-import type { Query, Mutation } from "@/generated/graphql"
-
 export enum LOCAL_STORAGE_KEYS {
   TOKEN = "TOKEN",
   HAS_WEBSOCKET_ENABLED = "HAS_WEBSOCKET_ENABLED",
   CELL_SIZE = "CELL_SIZE",
 }
-
-export type GRAPHQL_QUERY_KEYS = keyof Omit<Query & Mutation, "__typename">
 
 export const MAX_CHARACTERS_IN_CHAT = 50
 
