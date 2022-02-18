e0f5497325b011c44376c016b19952534c7218ab
Phillip ED Maier <pmaier983@gmail.com>
2022-02-17 22:19:25 -0800

    Remove Chat

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,9 @@
-import { MixedCheckbox } from "@reach/checkbox"
-import { useState } from "react"
 import styled from "styled-components"
-
-import { LOCAL_STORAGE_KEYS } from "@/utils/constants"
-import { toggleWebsocketsEnabled } from "@/utils/helperFunctions"
-import { ChatBox } from "@/components"
 
 const StyledWrapper = styled.div`
   grid-area: footer;
 `
 
 export const Footer = () => {
-  const [checked, setChecked] = useState(
-    !!window.localStorage.getItem(LOCAL_STORAGE_KEYS.HAS_WEBSOCKET_ENABLED)
-  )
-
-  return (
-    <StyledWrapper>
-      Enable Chat
-      <MixedCheckbox
-        checked={checked}
-        onChange={({ target: { checked } }) => {
-          setChecked(checked)
-          toggleWebsocketsEnabled(checked)
-        }}
-      />
-      {checked && <ChatBox />}
-    </StyledWrapper>
-  )
+  return <StyledWrapper></StyledWrapper>
 }
